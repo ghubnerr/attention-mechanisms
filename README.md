7e3f8dbbfd405d1f331b5d3d6c39e9ace8da37ea
Gabriel H. Lucchesi <gabrielhubnerlucchesi@gmail.com>
2025-03-21 17:03:39 -0400

    Add initial folder structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# A Survey on State-of-the-Art Attention Mechanisms
+
+### Tasks:
+
+- [ ] Translate Notebook into Repo
+- [ ] Notebook transformer block
+- [ ] Model sharding
+- [ ] Dataset
+- [ ] Evals
+- [ ] Add more...
---
+++ attention-mechanisms/configs/minimax_config.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+@dataclass
+class MiniMaxConfig:
+    # MoE
+    ffw_hidden_size: int = 9216
+    num_experts: int = 32
+    top_k: int = 2  # top-2 routing on MoE
+    aux_loss_coef: float = 0.01
+
+    # Attn
+    num_heads: int = 64
+    head_dim: int = 128
+    group_size: int = 8  # for Group-Query Attention
+    num_layers: int = 80
+    linear_per_softmax: int = 7 # 7 transnormers, then 1 transformer
+    hidden_size: int = 6144
+
+    deepnorm_alpha: float = 0.81
+
+    # RoPE
+    rope_fraction: float = 0.5
+    rope_base_freq: float = 10000
